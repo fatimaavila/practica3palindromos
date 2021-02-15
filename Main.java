c8a021a9810f0dbaafd202f07719fcdbcb4ef4ba
Fatima Avila <fatimaavila@ufm.edu>
2021-02-15 05:04:54 +0000

    Se convirtieron a métodos varias funcionalidades para el profiling

--- Main.java
+++ Main.java
@@ -2,19 +2,9 @@ import java.util.Scanner;
 import java.util.Arrays; 
 
 class Main {
-  public static void main(String[] args) {
-    String palabras;
-    
-     Scanner sc=new Scanner(System.in);
-        System.out.print("::Ingrese las palabras que quiere comprobar si son palíndromas o no. En una lista separada por comas::\n"+"\u001B[33m"+"EJEMPLO: hola,people,que,lol,oops,reloj\n\t"+"\u001B[0m"); 
-        
-        palabras=sc.nextLine();
-        String[] words = palabras.split(",");
-        //System.out.println(Arrays.toString(words));
-        System.out.println("\n");
-        
-        for(int k=0; k < words.length; k++) {
-               char[] palindroma = words[k].toCharArray();
+
+  private static String reverse(String palabra ) {
+         char[] palindroma = palabra.toCharArray();
               // System.out.println(Arrays.toString(palindroma));
                char[] reversa = new char[palindroma.length];
                int l = 0;
@@ -28,7 +18,25 @@ class Main {
     } // for palindroma
     //System.out.println(Arrays.toString(reversa));
      
-     String pali = new String(reversa);
+    return new String(reversa);
+}
+
+
+  
+  public static void main(String[] args) {
+    String palabras;
+    
+     Scanner sc=new Scanner(System.in);
+        System.out.print("::Ingrese las palabras que quiere comprobar si son palíndromas o no. En una lista separada por comas::\n"+"\u001B[33m"+"EJEMPLO: hola,people,que,lol,oops,reloj\n\t"+"\u001B[0m"); 
+        
+        palabras=sc.nextLine();
+        String[] words = palabras.split(",");
+        //System.out.println(Arrays.toString(words));
+        System.out.println("\n");
+        
+        for(int k=0; k < words.length; k++) {
+         
+     String pali = reverse(words[k]);
       String mensaje = "";
      if(pali.equals(words[k])){
       mensaje = "\u001B[36m"+"Palabra palíndroma"+"\u001B[0m";
